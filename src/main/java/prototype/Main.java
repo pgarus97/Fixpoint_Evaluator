e97bf22d660731f17f04f933710ea00b6a25a012
Philip Garus <philipgarus97@gmail.com>
2022-05-12 20:39:45 +0200

    changed calculation to string conversion method, implemented first conditionals, sequentials and assignments

--- src/main/java/prototype/Main.java
+++ src/main/java/prototype/Main.java
@@ -22,17 +22,16 @@ public class Main {
 		System.out.println(wp(C,f));
 	}
 	
+	public static double calculation(String exp) {
+		Expression e = new Expression(exp);
+		System.out.println("Expression:" + exp);
+		System.out.println("Calculation Result: " + e.calculate());
+		return e.calculate();
+	}
+	
 	//first example: x = 5 ; if(x=5){x = x+1} else {x = x-1}
-	public static double wp(String C, String f) {
-		//TODO here we need to parse C and select the right submethods
-		// first step is cut ; into multiple Cs
-		System.out.println("wp["+C+"]("+f+")" );
-		
-		//nested example: if(x==5){if(y==5){y= y+1} else {y = y-1}} else { x = x+1}
-		//second step {if(y==5){y= y+1} else {y = y-1}} else { x = x+1}
-		// push if(y==5){y= y+1} else {y = y-1}} else { x = x+1} 0
-		// push y = y+1} else {y = y-1}} else { x = x+1}
-		
+	public static String wp(String C, String f) {
+		System.out.println("wp["+C+"]("+f+")" );	
 		
 		if(C.startsWith(" if ")) {
 			System.out.println("Enter conditional if"); 
@@ -40,65 +39,69 @@ public class Main {
 			String condition = C.substring(C.indexOf("(")+1,C.indexOf(")"));
 			System.out.println("Conditional: "+condition); 
 			int bracketCount = 1;
-			String C1 = C.substring(C.indexOf("{")+1);
-			System.out.println("C1 before for: " + C1); 
-			//TODO rewrite with 
-			/*
-			 * for(int i = 0; i < s.length(); i++)
-				{
-				   char c = s.charAt(i);
+			String C1 = "";
+			String tempC1 = C.substring(C.indexOf("{")+1);
+
+			for(int i = 0; i < tempC1.length(); i++) {
+				char character = tempC1.charAt(i);
+				if(character == '{') {
+					bracketCount++;
 				}
-			 */
-			for (char character : C1.toCharArray()) {
+				if(character == '}') {
+					bracketCount--;
+				}
 				if(bracketCount != 0) {
-					if(character == '{') {
-						bracketCount++;
-						System.out.println("brackets +"); 
-					}
-					if(character == '}') {
-						bracketCount--;
-					}
 					C1 = C1 + character;
 				}else {
 					break;
 				}
 			}
-			String C2 = C.substring(C.indexOf(C1));
-			C2 = C2.substring(C2.indexOf("{")+1);
+			String C2 = "";
+			String tempC2 = C.substring(C.indexOf(C1));
+			tempC2 = tempC2.substring(tempC2.indexOf("{")+1);
 			bracketCount++;
-			for (char character2 : C2.toCharArray()) {
+			for(int i = 0; i < tempC2.length(); i++) {
+				char character = tempC2.charAt(i);
+				if(character == '{') {
+					bracketCount++;
+				}
+				if(character == '}') {
+					bracketCount--;
+				}
 				if(bracketCount != 0) {
-					if(character2 == '{') {
-						bracketCount++;
-					}
-					if(character2 == '}') {
-						bracketCount--;
-					}
-					C2 = C2 + character2;
+					C2 = C2 + character;
 				}else {
 					break;
 				}
 			}
 			System.out.println("C1= "+C1); 
 			System.out.println("C2= "+C2); 
-			System.out.println("Breaking down into: if("+condition+") then "+ wp(C1,f)+" else "+ wp(C2,f)); 
-			Expression e = new Expression("if("+condition+"),"+ wp(C1,f)+","+ wp(C2,f)+")");
-			return e.calculate();
+			String resultC1 = wp(C1,f);
+			String resultC2 = wp(C2,f);
+			System.out.println("Breaking down into: if("+condition+") then "+ resultC1 +" else "+ resultC2); 
+			//TODO no idea what happens in this case. Like page 10 but no x to evaluate phi
+			String result = "if("+condition+","+ resultC1 + "," + resultC2 + ")";
+
+			return result;
 		}
 		
-		//wont work, need to have priority on other operators
 		else if(C.contains(";")) {
 			System.out.println("Enter sequential if"); 
 
 			String C1 = C.substring(0, C.indexOf(";")-1);
 			String C2 = C.substring(C.indexOf(";")+1);
-			System.out.println("Breaking down into: wp["+C1+"](wp[+"+C2+"]("+f+"))"); 
-			return (wp(C1, Double.toString(wp(C2,f))));
+			System.out.println("Breaking down into: wp["+C1+"](wp["+C2+"]("+f+"))"); 
+			return wp(C1,(wp(C2,f)));
 		}else {
-			Argument x = new Argument(C);
-			Expression e = new Expression(f, x);
+			// TODO probably need new assignment functionality as variables need to be stored separately to access f
+			// TODO wp[x=x+1](x^2) should result in (x+1)^2 String without calculation.
+			// TODO calculation should only happen if we have a non dependent assignment
+			// TODO on lowest recursion point we need to merge and fix together the strings that we will return later as result of wp
+		
+			//TODO check here if variable contains only numbers x = 5 or also dependent variables. Actually maybe not, just string it together somehow?
 			
-			double result = e.calculate();
+			String cutC = C.substring(C.indexOf("=")+1);
+			String result = f.replace("x", "("+cutC+")");
 			System.out.println(result);
 			return result;
 		}
