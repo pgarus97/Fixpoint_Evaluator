3f13aba87bd300d1402e59bb5657c373a15dd2e1
pgarus97 <philipgarus97@gmail.com>
2022-05-21 22:09:59 +0200

    fixed sequential process to new initial parsing system

--- src/main/java/prototype/Main.java
+++ src/main/java/prototype/Main.java
@@ -1,5 +1,6 @@
 package prototype;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 import org.mariuszgromada.math.mxparser.Argument;
@@ -16,7 +17,7 @@ public class Main {
 	    System.out.print("Enter initial variable assignments: (Multiple possible: e.g. 'x=5;y=3;z=2");
 	    String sigma = input.nextLine();
 	    if(sigma.isEmpty()) {
-	    	//throw expection;
+	    	//throw exception;
 	    	System.out.println("You have to input an initial variable assignment");
 	    	return;
 	    }
@@ -27,13 +28,15 @@ public class Main {
 	    String f = input.nextLine();
 	    C = sigma+C;
 
-
+	    //TODO new persistent variable implementation
+	    HashMap<String, String> variables = new HashMap<String,String>();
 		System.out.println("wp["+C+"]("+f+") =" );
 		//Variables variables = new Variables(inputtedVariables);
 		//append variables C1 = x=5 etc. as C1;C2;C3;C
 		System.out.println(wp(C,f));
 	}
 	
+	//TODO probably obsolete if we require initial variable assignments
 	public static String calculation(String exp) {
 		Expression e = new Expression(exp);
 		System.out.println("Expression:" + exp);
@@ -47,142 +50,130 @@ public class Main {
 		}
 	}
 	
-	//first example: x = 5 ; if(x=5){x = x+1} else {x = x-1}
+	public static String getInsideBracket(String C) {
+		int bracketCount = 1;
+		String result = "";
+		for(int i = 0; i < C.length(); i++) {
+			char character = C.charAt(i);
+			if(character == '{') {
+				bracketCount++;
+			}
+			if(character == '}') {
+				bracketCount--;
+			}
+			if(bracketCount != 0) {
+				result = result + character;
+			}else {
+				break;
+			}
+		}
+		return result;
+	}
+	
+	public static String getSequentialCut(String C) {
+		int bracketCount = 0;
+		String result = "";
+		for(int i = 0; i < C.length(); i++) {
+			char character = C.charAt(i);
+			if(character == '{') {
+				bracketCount++;
+			}
+			if(character == '}') {
+				bracketCount--;
+			}
+			if(character == ';' && bracketCount == 0) {
+				break;
+			}else {
+				result = result + character;
+			}
+		}
+		return result;
+	}
+	
+	
 	public static String wp(String C, String f) {
 		C = C.replace(" ", "");
-
-		System.out.println("wp["+C+"]("+f+")" );	
-		
-		if(C.startsWith("if")) {
-			//conditional process
-			System.out.println("Enter conditional process"); 
-			String condition = C.substring(C.indexOf("(")+1,C.indexOf(")"));
-			System.out.println("Conditional: "+condition); 
-			int bracketCount = 1;
-			String C1 = "";
-			String tempC1 = C.substring(C.indexOf("{")+1);
-			//TODO make a separate method for getInsideBracket(String)
-			for(int i = 0; i < tempC1.length(); i++) {
-				char character = tempC1.charAt(i);
-				if(character == '{') {
-					bracketCount++;
-				}
-				if(character == '}') {
-					bracketCount--;
-				}
-				if(bracketCount != 0) {
-					C1 = C1 + character;
-				}else {
-					break;
-				}
-			}
-			
-			String C2 = "";
-			String tempC2 = C.substring(C.indexOf(C1));
-			tempC2 = tempC2.substring(tempC2.indexOf("{")+1);
-			bracketCount++;
-			for(int i = 0; i < tempC2.length(); i++) {
-				char character = tempC2.charAt(i);
-				if(character == '{') {
-					bracketCount++;
-				}
-				if(character == '}') {
-					bracketCount--;
-				}
-				if(bracketCount != 0) {
-					C2 = C2 + character;
-				}else {
-					break;
-				}
-			}
-			System.out.println("C1= "+C1); 
-			System.out.println("C2= "+C2);
-			String resultC1 = wp(C1,f);
-			String resultC2 = wp(C2,f);
-			System.out.println("Breaking down into: if("+condition+") then "+ resultC1 +" else "+ resultC2); 
-			String result = "if("+condition+","+ resultC1 + "," + resultC2 + ")";
-
-			return result;
-		}
-		
-		else if(C.startsWith("{")){
-			//probability process
-			System.out.println("Enter probability process"); 
-			
-			int bracketCount = 1;
-			String C1 = "";
-			String tempC1 = C.substring(C.indexOf("{")+1);
-
-			for(int i = 0; i < tempC1.length(); i++) {
-				char character = tempC1.charAt(i);
-				if(character == '{') {
-					bracketCount++;
-				}
-				if(character == '}') {
-					bracketCount--;
-				}
-				if(bracketCount != 0) {
-					C1 = C1 + character;
-				}else {
-					break;
-				}
-			}
-			String C2 = "";
-			String tempC2 = C.substring(C.indexOf(C1));
-			String probability = tempC2.substring(tempC2.indexOf("[")+1,C.indexOf("]")-1);
-			tempC2 = tempC2.substring(tempC2.indexOf("{")+1);
-			bracketCount++;
-			for(int i = 0; i < tempC2.length(); i++) {
-				char character = tempC2.charAt(i);
-				if(character == '{') {
-					bracketCount++;
-				}
-				if(character == '}') {
-					bracketCount--;
-				}
-				if(bracketCount != 0) {
-					C2 = C2 + character;
-				}else {
-					break;
-				}
-			}
-			System.out.println("C1= "+C1); 
-			System.out.println("C2= "+C2);
-			System.out.println("Probability:" + probability);
-			Expression negprobability = new Expression ("1-"+probability);
-			String resultC1 = wp(C1,f);
-			String resultC2 = wp(C2,f);
-			System.out.println("Breaking down into: "+probability+" * "+ resultC1 +" + "+ negprobability.calculate() +" * "+ resultC2); 
-			String result = probability+" * "+resultC1+" + "+negprobability.calculate()+" * "+resultC2;
-
-			return result;
-		}
-		
-		else if(C.contains(";")) {
-			//sequential process
+		System.out.println("wp["+C+"]("+f+")" );
+		System.out.println("C "+ C);
+		String C1 = getSequentialCut(C);
+		System.out.println("C1: " + C1);
+		if(!C1.equals(C)) {
 			System.out.println("Enter sequential process"); 
-
-			String C1 = C.substring(0, C.indexOf(";"));
-			String C2 = C.substring(C.indexOf(";")+1);
+			String C2 = C.substring(C1.length()+1);
+			System.out.println("C2: " + C2);
 			System.out.println("Breaking down into: wp["+C1+"](wp["+C2+"]("+f+"))"); 
 			return wp(C1,(wp(C2,f)));
 		}else {
-			//variable assignments
-			System.out.println("Enter assignment process"); 
-			
-			if(C.contains("skip")){
-				String result = C.replace("skip", f);
-				System.out.println(result);
-				return result;
-			}else {
-				String indexC = C.substring(0,1);
-				String cutC = C.substring(C.indexOf("=")+1);
-				String result = f.replace(indexC, "("+cutC+")");
-				System.out.println(result);
+			if(C.startsWith("if")) {
+				//conditional process
+				System.out.println("Enter conditional process"); 
+				String condition = C.substring(C.indexOf("(")+1,C.indexOf(")"));
+				System.out.println("Conditional: "+condition); 
+				String ifC1 = getInsideBracket(C.substring(C.indexOf("{")+1));	
+				String ifC2 = C.substring(C.indexOf(ifC1));
+				ifC2 = getInsideBracket(ifC2.substring(ifC2.indexOf("{")+1));
+				
+				System.out.println("C1= "+ifC1); 
+				System.out.println("C2= "+ifC2);
+				String resultC1 = wp(ifC1,f);
+				String resultC2 = wp(ifC2,f);
+				System.out.println("Breaking down into: if("+condition+") then "+ resultC1 +" else "+ resultC2); 
+				String result = "if("+condition+","+ resultC1 + "," + resultC2 + ")";
+				
 				return result;
 			}
 			
+			else if(C.startsWith("{")){
+				//probability process
+				System.out.println("Enter probability process"); 
+				String probC1 = getInsideBracket(C.substring(C.indexOf("{")+1));
+
+				String probC2 = C.substring(probC1.length());
+				String probability = probC2.substring(probC2.indexOf("[")+1,probC2.indexOf("]"));
+				probC2 = getInsideBracket(probC2.substring(probC2.indexOf("{")+1));
+
+				System.out.println("C1= "+probC1); 
+				System.out.println("C2= "+probC2);
+				System.out.println("Probability:" + probability);
+				Expression negProbability = new Expression ("1-"+probability);
+				String resultC1 = wp(probC1,f);
+				String resultC2 = wp(probC2,f);
+				System.out.println("Breaking down into: "+probability+" * "+ resultC1 +" + "+ negProbability.calculate() +" * "+ resultC2); 
+				String result = probability+" * "+resultC1+" + "+negProbability.calculate()+" * "+resultC2;
+
+				return result;
+			}
+			else if(C.startsWith("while")){
+				//while process TODO
+				System.out.println("Enter while process"); 
+				String condition = C.substring(C.indexOf("(")+1,C.indexOf(")"));
+				System.out.println("Condition: "+condition);
+						//parse "if condition, then take f, else wp C 0 ? with X(0) = f = 0?
+				String result = "";
+
+				return result;
+				
+			}else {
+				//variable assignments
+				System.out.println("Enter assignment process"); 
+				
+				if(C.contains("skip")){
+					String result = C.replace("skip", f);
+					System.out.println(result);
+					return result;
+				}else {
+					String indexC = C.substring(0,1);
+					String cutC = C.substring(C.indexOf("=")+1);
+					String result = f.replace(indexC, "("+cutC+")");
+					System.out.println(result);
+					return result;
+				}
+				
+			}
+		
 		}
-	}
+		
+	}	
+		
 	
 }
--- src/test/java/prototype/MainTest.java
+++ src/test/java/prototype/MainTest.java
@@ -1,12 +1,19 @@
 package prototype;
 
 
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
 
 class MainTest {
+	
+    HashMap<String, String> variables = new HashMap<String,String>();
+
 
 	@Test
 	void testAssignments() {
+		
+		variables.put("x", "5");
 		
 		//assignments
 		assert Main.calculation(Main.wp("x=5", "x^2")).equals("25.0");
@@ -21,16 +28,20 @@ class MainTest {
 		
 		//probability with initial assignment x=5
 		assert Main.calculation(Main.wp("{x=5}[4/5]{x=10}","x^2")).equals("40.0");
-		assert Main.calculation(Main.wp("x=5;{x=5}[4/5]{x=10}","x^2")).equals("40.0");
+		assert Main.calculation(Main.wp("{x=5}[4/5]{x=10};x=6","x^2")).equals("36.0");
 		assert Main.calculation(Main.wp("{skip}[1/2]{x=x+2}","x")).equals("1/2 * x + 0.5 * (x+2)");
 		assert Main.calculation(Main.wp("x=5;{skip}[1/2]{x=x+2}","x")).equals("6.0");
 
 	}
 	
 	@Test
-	void testMixed() {
-		
-		assert Main.calculation(Main.wp("x=5 ; if (x<5) {x=x+1} else {x=x-1}", "x^2")).equals("16.0");
-		// assert Main.calculation(Main.wp("if (x<5) {x=x+1} else {x=x-1}", "x^2")).equals("?"); What would this compute to?
+	void testConditional() {	
+		assert Main.calculation(Main.wp("x=5;if (x<5) {x=x+1} else {x=x-1}", "x^2")).equals("16.0");
+	}
+	
+	@Test
+	void testWhile() {	
+		//assert Main.calculation(Main.wp("c=0;x=0; while(c=1){x=x+1}[1/2]{c=0}", "x")).equals("0.0");
+		//assert Main.calculation(Main.wp("c=0;x=0; if (x<5) {x=x+1} else {x=x-1}", "x^2")).equals("16.0");
 	}
 }
